db6cd24048dbb6f5913dfc23bfb42e2545472d59
trampitax <trampitaxonfire@gmail.com>
2019-10-17 10:09:27 +0200

    feat(hanged.view.js): loads html and also create each letter button

---
+++ src/controllers/hanged.controller.js
@@ -0,0 +1,5 @@
+class HangedController {
+  constructor(_view) {
+    this.view = _view;
+  }
+}
---
+++ src/views/hanged.view.js
@@ -0,0 +1,21 @@
+class HangedView {
+  constructor() {
+    fetch('/src/views/hanged.view.html')
+      .then(response => response.text())
+      .then(html => {
+        const hangedViewHTML = new DOMParser().parseFromString(html, 'text/html');
+        document.getElementById('root').innerHTML = hangedViewHTML.body.innerHTML;
+      })
+      .then(() => {
+        this.dom = this.cacheDom();
+        letters.forEach(element => (this.dom.panelLetters.appendChild(document.createElement('button')).innerHTML = element));
+      });
+  }
+
+  cacheDom = () => {
+    return {
+      canvas: document.getElementById('myCanvas'),
+      panelLetters: document.getElementById('panelLetters')
+    };
+  };
+}
